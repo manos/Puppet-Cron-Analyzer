6d0dad37ba681b19743205343dd4ab86391b4ab2
Charlie Schluting <charlie@>
2012-05-26 20:04:01 -0700

    simplify, the complexity of args wasn't necessary at ALL.

--- cron-parse.py
+++ cron-parse.py
@@ -8,30 +8,26 @@
 #  digestion by cron-analyze. The [input file] (or stdin) is expected to be a puppet
 #  catalog in json format (see `puppet master --compile`).
 #
-# usage: cron-parse.py [options] [input files...] [output file]
+# usage: cron-parse.py [--console]
 #
-# no options, no arguments, with stdin provided: reads stdin and outputs to stdout
-# no options with stdin provided, and one file arguments: reads stdin, and outputs to file
-# no options, one file argument: reads from file, outputs to stdout
-# no options, > 1 file argument: reads all files except last one; outputs to last file, unless --stdout is used
-# --dest option, reads from either stdin or all file arguments, and outputs to --dest
+# With no command line options, this will parse all crons in ./catalogs/ and output to ./output/.
+# If stdin is provided, it will output the result to stdout.
 #
 # If --generate is used, we will attempt to ask puppet to compile all catalogs (via puppet.py).
 # Useful if you don't want to manage importing puppet catalogs' json into this script.
 # It fetches a list of all not-revoked puppet certs, and then calls
 #  'puppet master --compile' on each. Must be run on the puppet master, with sudo privs
 #  for running the puppet cert and master commands.
-# This can take a long time!
+# This can take a long time! (TODO: parallel compilation? Or sleeping, to not overwhelm the master?)
 
 import sys, os, logging
 import simplejson as json
 from optparse import OptionParser
 
 # parse arguments
-parser = OptionParser("usage: %prog [options] [output file]")
-parser.add_option("--dest", default=None, help="destination file for output (stdout if not used)")
-parser.add_option("--stdout", default=False, action="store_true", help="always output to stdout")
-parser.add_option("--debug", default=None, help="enable debug output")
+parser = OptionParser("usage: %prog [options]")
+parser.add_option("--console", default=False, action="store_true", help="output to stdout")
+parser.add_option("-d", "--debug", default=None, help="enable debug output")
 parser.add_option("-g", "--generate", default=None, help="run puppet master --compile to generate json catalogs")
 (options, args) = parser.parse_args()
 
@@ -55,51 +51,39 @@ def extract_crons(puppet_catalog):
                 crons.append(resource)
     return crons
 
+def do_parse_and_write(catalogs_dir, outdir):
+    for catalog in os.listdir(catalogs_dir):
+        # parse crons out of catalog
+        crons = extract_crons(json.load(open(catalogs_dir + catalog)))
+
+        # write json of just crons
+        FILE = open(outdir + catalog, 'w')
+        print >>FILE, json.dumps(crons)
+        FILE.close()
+    return True
+
 if __name__ == '__main__':
+    outdir = './output/'
+    input_dir = './catalogs/'
 
     stdin = None
     if not sys.stdin.isatty(): # redirected from file or pipe
         stdin = sys.stdin.read()
 
-    if len(args) < 1 and not stdin:
-        parser.error("must supply at least one file name argument, or use stdin")
-
-    ''' Build a list of cron resources from stdin or the list of files passed as args. '''
+    ''' Build a list of cron resources from stdin or from input_dir '''
     crons = []
-
-    # TODO: add "host": $hostname to the dict for each cron.
 
     if stdin:
         crons = extract_crons(json.loads(stdin))
-    elif not options.generate and len(args) == 1:
-        for catalog in args:
-            crons += extract_crons(json.load(open(catalog)))
-    elif not options.generate and len(args) > 1:
-        if options.stdout:
-            _args = args
-        else:
-            _args = args[:-1]
-        for catalog in _args:
-            crons += extract_crons(json.load(open(catalog)))
+    elif options.generate:
+        ''' TODO generate catalogs '''
+        do_parse_and_write(input_dir, outdir)
     else:
-        ''' TODO generate catalogs! '''
+        do_parse_and_write(input_dir, outdir)
 
-    out_file = None
-    if options.dest and not options.stdout:
-        out_file = options.dest
-    elif not stdin and not options.stdout and len(args) == 1:
-        out_file = None
-    elif stdin and not options.stdout and len(args) == 1:
-        out_file = args[0]
-    elif not options.stdout and len(args) > 1:
-        out_file = args[-1:][0]
-
-    if out_file:
-        FILE = open(out_file, 'w')
-        print >>FILE, json.dumps(crons)
-        FILE.close()
-    else:
+    if stdin:
         print json.dumps(crons)
+
 
 
 
